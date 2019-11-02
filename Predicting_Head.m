ca22d42f7ee3b54ab8ea32fd3ef5024d3933e9e4
Nabeel Hussain <31992723+nabeel3133@users.noreply.github.com>
2019-11-02 10:39:50 +0500

    Add files via upload

---
+++ Predicting_Head.m
@@ -0,0 +1,12 @@
+%% PREDICTING HEAD
+load('LYHM_male.mat')
+load('Whf700Male.mat')
+load('Face.mat')
+head_mean = transpose(shp.mu);
+head_U = shp.eigVec;
+load('BFM.mat')
+face_mean = shapeMU;
+face_U = shapePC;
+input_face = transpose(Face_verts);
+pred_head = head_mean + (head_U * (Whf * (transpose(face_U)*(input_face - face_mean))));
+%% ---------------
