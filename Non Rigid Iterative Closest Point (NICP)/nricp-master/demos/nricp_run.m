f6f0c75df6edc7c76704f4b5854fbae9226226da
Nabeel Hussain <31992723+nabeel3133@users.noreply.github.com>
2019-12-05 12:19:30 +0500

    Add files via upload

--- Non Rigid Iterative Closest Point (NICP)/nricp-master/demos/nricp_run.m
+++ Non Rigid Iterative Closest Point (NICP)/nricp-master/demos/nricp_run.m
@@ -3,7 +3,7 @@
 load ../data/Head_Tri.mat
 load ../data/Target_MeanFace_Cropped.mat
 
-% Specify that surface normals are available and can be used.
+% Specify that surface normals need not to be used.
 Options.useNormals = 0;
 
 % Specify that the source deformations should be plotted.
@@ -12,7 +12,7 @@ Options.plot = 1;
 Options.gamm = 1;   %Default 1
 Options.epsilon = 1e-4;   %Default 1e-4
 Options.lambda = 1;  %Default 1
-Options.alphaSet = linspace(1000, 100, 15);  %Default linspace(100,10,20)
+Options.alphaSet = linspace(100, 10, 20);  %Default linspace(100,10,20)
 Options.biDirectional = 0;  %Default 0
 Options.rigidInit = 1;   %Default 1
 Options.ignoreBoundary = 1;  %Default 1
@@ -34,5 +34,31 @@ for i=1:total_heads
     registered_heads(:,:,i) = pointsTransformed;
 end
 %% --------------------------------------------------
+
+
+%% PROJECTING HEAD AND GENERATING CORRESPONDING FACE
+proj_X = zeros(length(face_U),1,total_heads);
+for i=1:total_heads
+    X = reshape(transpose(registered_heads(:,:,i)),[length(head_U),1]);
+    ph=(transpose(head_U(:,model_numbers(i,1))) * (X - head_mean)); 
+    ph = repmat(ph, [length(face_U(1,:)),1]);
+    proj_X(:,:,i) = face_mean + face_U*ph;
+end
+%% --------------------------------------------------
+
+
+%% GET PARAMETERS OF FACE USING ENTIRE EIGENSPACE
+Cf = zeros(length(face_U(1,:)),total_heads);
+for i=1:total_heads
+    Cf(:,i) = transpose(face_U) * (proj_X(:,:,i) - face_mean);
+end
+%% --------------------------------------------------
+
+
+%% CONSTRUCTING REGRESSION MATRIX AND SAVING IT IN MAT FILE
+Whf = Ch * transpose(Cf)* inv(Cf*transpose(Cf));     % Equation (5) of the paper
+save('../../../Regression Matrix Calculation/Regression_Matrix_Test','Whf')
+%% --------------------------------------------------
+
 
 
